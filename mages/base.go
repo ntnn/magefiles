8eb6d41ad2e41e16ec105420a25f1480600a06bb
Nelo-T. Wallus <nelo@wallus.de>
2023-08-15 02:42:20 +0200

    Add docs and PreGenerateDeletePatterns / PreGenerate

--- mages/base.go
+++ mages/base.go
@@ -9,16 +9,64 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
+// PreGenerateDeletePatterns are used in Generate to automatically
+// delete generated files before generation using find.
+// These can be set when importing the package, e.g.
+//
+// package main
+//
+// import (
+//
+//	//mage:import
+//	"github.com/ntnn/magefiles/mages"
+//
+// )
+//
+//	func init() {
+//		mages.PreGenerateDeletePatterns = []string{
+//			"_gen.go",
+//		}
+//	}
+var PreGenerateDeletePatterns = []string{
+	"*.gen.go",
+	"*.gen_test.go",
+	"*_gen.go",
+	"*_gen_test.go",
+}
+
+// preGenerateDelete is automatically run as a dependency of Generate
+// and removes files matching patterns from PreGenerateDeletePatterns
+// using `find`.
+func preGenerateDelete() error {
+	for _, pattern := range PreGenerateDeletePatterns {
+		if err := sh.RunV("find", ".", "-type", "f", "-name", pattern, "-delete"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// PreGenerate allows to add more dependencies to Generate.
+var PreGenerate = []any{}
+
 func Generate() error {
+	mg.Deps(append(PreGenerate, preGenerateDelete)...)
 	return sh.RunV("go", "generate", "./...")
 }
 
+// Check runs golangci-lint.
 func Check() error {
 	return sh.RunV("go", "run", "github.com/golangci/golangci-lint/cmd/golangci-lint", "run", "./...")
 }
 
+// Lint runs golangci-lint with the given argument.
+// Due to a limitation in mage the argument has to be quoted:
+// https://github.com/magefile/mage/issues/340 is resolved
+//
+// E.g.:
+//
+//	go run ./mage.go default:lint 'completion zsh'
 func Lint(subcmd string) error {
-	// TODO until https://github.com/magefile/mage/issues/340 is resolved
 	return sh.RunV(
 		"go",
 		append(
@@ -28,10 +76,13 @@ func Lint(subcmd string) error {
 	)
 }
 
+// Test runs `go test` with coverage and parallelism enabled.
+// Parallelism is set to the number of available CPUs.
 func Test() error {
 	return sh.RunV("go", "test", "-parallel", fmt.Sprintf("%d", runtime.NumCPU()), "-cover", "./...")
 }
 
+// All runs generate, check and test in order.
 func All() {
 	mg.SerialDeps(Generate, Check, Test)
 }
